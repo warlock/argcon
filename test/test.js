c907fb91673d830cfd30809c6646ce416c927ce9
Josep Subils <js@js.gl>
2017-11-30 09:31:42 +0100

    [test] Add playing script

--- test/test.js
+++ test/test.js
@@ -1,3 +1,18 @@
+#!/usr/bin/env node
 const arg = require('../index.js')
 
-arg.get(['-a', 'b', '-c', 'd', 'e'])
+arg.on('-a', res => {
+  if (res) console.log(`Response -a: ${res}`)
+})
+
+arg.on('-b', res => {
+  if (res) console.log(`Response -a: ${res}`)
+})
+
+arg.on('-c', res => {
+  if (res) console.log(`Response -a: ${res}`)
+})
+
+arg.on(res => {
+  console.log(`Response alone: ${JSON.stringify(res)}`)
+})
